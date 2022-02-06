f518216fa74967acebdea42be96b11ac59dbba12
Joshduncan89 <51183094+Joshduncan89@users.noreply.github.com>
2022-02-06 14:30:00 -0500

    Removed express async handler and updated Database

--- backEnd/config/db.js
+++ backEnd/config/db.js
@@ -1,16 +1,26 @@
 import mongoose from "mongoose";
 
+// const connectDb = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI, {
+//       useUnifiedTopology: true,
+//       useNewUrlParser: true,
+//       useCreateIndex: true,
+//     });
+
+//     console.log("MongoDb connected");
+//   } catch (err) {
+//     console.log(err.message);
+//     process.exit(1);
+//   }
+// };
+
 const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    });
-
-    console.log("MongoDb connected");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected...");
   } catch (err) {
-    console.log(err.message);
+    console.error(err.message);
     process.exit(1);
   }
 };
